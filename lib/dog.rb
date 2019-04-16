5a62778d51dd88fb5b1525129d55f10a235db41a
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-04-16 02:19:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,9 +62,9 @@ class Dog
 
   def new_from_db(row)
     attributes_hash = {
-      :id = row[0],
-      :name = row[1],
-      :breed = row[2]
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
     }
     self.new(attributes_hash)
 
